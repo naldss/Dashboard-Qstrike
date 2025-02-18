1efdd00310f4cace5d7550b6670956075fafe0f5
Naldwin_SEO <n.cuengco@qstrike.com>
2025-02-18 13:31:44 +0800

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { createPinia } from "pinia";
 
-createApp(App).mount('#app')
+// UIkit
+import "uikit/dist/css/uikit.min.css";
+import "uikit/dist/js/uikit.min.js";
+
+// Vue Toastification
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
+const app = createApp(App);
+
+app.use(router);
+app.use(createPinia());
+app.use(Toast);
+
+app.mount("#app");
---
+++ src/router.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import LoginPage from './views/LoginPage.vue';
+
+const routes = [
+    {path: '/', redirect: '/login'},
+    { path: '/login', component: LoginPage },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
